4a68e83c9ed4206f3d2cc9a040b2d632faacfd7a
chandru <chandruviru143@gmail.com>
2024-10-06 21:37:49 +0530

    ransom note solution added

--- 383. Ransom Note.js
+++ 383. Ransom Note.js
@@ -24,11 +24,25 @@ Output: true
  * @return {boolean}
  */
 var canConstruct = function (ransomNote, magazine) {
-    for (const char of magazine) {
-        ransomNote = ransomNote.replace(char, "");
-      }
-      if (!ransomNote) return true;
-      else return false;
+  // approach 1
+  const counter = {};
+  for (let i = 0; i < magazine.length; i++) {
+    counter[magazine[i]] = (counter[magazine[i]] || 0) + 1;
+  }
+  for (let j = 0; j < ransomNote.length; j++) {
+    if (!counter[ransomNote[j]]) return false;
+    else counter[ransomNote[j]] -= 1;
+  }
+
+  return true;
+
+  //approach 2
+
+  for (const char of magazine) {
+    ransomNote = ransomNote.replace(char, "");
+  }
+  if (!ransomNote) return true;
+  else return false;
 };
 
 console.log(canConstruct("a", "b"))
